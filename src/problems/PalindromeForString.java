8fadaa82420fee4b75154232409c3ed1def23798
yuliya.hancharenka <Creator13>
2022-02-07 20:46:43 +0300

    Add leetcode tasks

---
+++ src/problems/PalindromeForString.java
@@ -0,0 +1,18 @@
+package problems;
+
+public class PalindromeForString {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("asdfdsa"));
+    }
+
+    public static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
