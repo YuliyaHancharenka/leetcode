92b8928b9c538acf07d3a7ce5fc0fdd13b5f5063
yuliya.hancharenka <Creator13>
2022-02-12 18:22:29 +0300

    Add leetcode tasks

---
+++ src/problems/FloodFill.java
@@ -0,0 +1,39 @@
+package problems;
+
+public class FloodFill {
+    //733 //todo not clear
+    public static void main(String[] args) {
+        int[][] image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+        int sr = 1, sc = 1, newColor = 2;
+
+        int[][] result = floodFill(image, sr, sc, newColor);
+    }
+
+    //Approach #1: Depth-First Search
+    private static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int color = image[sr][sc];
+        if (color != newColor) dfs(image, sr, sc, color, newColor);
+        return image;
+    }
+
+    private static void dfs(int[][] image, int r, int c, int color, int newColor) {
+        if (image[r][c] == color) {
+            image[r][c] = newColor;
+            if (r >= 1) dfs(image, r - 1, c, color, newColor);
+            if (c >= 1) dfs(image, r, c - 1, color, newColor);
+            if (r + 1 < image.length) dfs(image, r + 1, c, color, newColor);
+            if (c + 1 < image[0].length) dfs(image, r, c + 1, color, newColor);
+        }
+    }
+}
+/*
+Example 1:
+Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, newColor = 2
+Output: [[2,2,2],[2,2,0],[2,0,1]]
+Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+
+Example 2:
+Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, newColor = 2
+Output: [[2,2,2],[2,2,2]]
+ */
---
+++ src/problems/LongestCommonPrefix.java
@@ -0,0 +1,71 @@
+package problems;
+
+public class LongestCommonPrefix {
+    //14
+    public static void main(String[] args) {
+        String[] strs = new String[]{"flower", "flow", "flight"};
+        System.out.println(longestCommonPrefixVerticalScanning(strs));
+    }
+
+    //Approach 1: Horizontal scanning
+    private static String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++)
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty()) return "";
+            }
+        return prefix;
+    }
+
+    //Approach 2: Vertical scanning
+    private static String longestCommonPrefixVerticalScanning(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c)
+                    return strs[0].substring(0, i);
+            }
+        }
+        return strs[0];
+    }
+
+    //Approach 3: Divide and conquer //todo not clear
+    private static String longestCommonPrefixDivideAndConquer(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        return longestCommonPrefix(strs, 0, strs.length - 1);
+    }
+
+    private static String longestCommonPrefix(String[] strs, int l, int r) {
+        if (l == r) {
+            return strs[l];
+        } else {
+            int mid = (l + r) / 2;
+            String lcpLeft = longestCommonPrefix(strs, l, mid);
+            String lcpRight = longestCommonPrefix(strs, mid + 1, r);
+            return commonPrefix(lcpLeft, lcpRight);
+        }
+    }
+
+    static String commonPrefix(String left, String right) {
+        int min = Math.min(left.length(), right.length());
+        for (int i = 0; i < min; i++) {
+            if (left.charAt(i) != right.charAt(i))
+                return left.substring(0, i);
+        }
+        return left.substring(0, min);
+    }
+}
+
+/*
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Example 2:
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ */
